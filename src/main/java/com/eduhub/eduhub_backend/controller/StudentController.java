ad0ebc8d25f4ba37f21d4c5a2310bef17b9e84f2
raghav <raghavvenkat18@gmail.com>
2026-05-26 14:13:14 +0530

    two

--- src/main/java/com/eduhub/eduhub_backend/controller/StudentController.java
+++ src/main/java/com/eduhub/eduhub_backend/controller/StudentController.java
@@ -3,8 +3,7 @@ package com.eduhub.eduhub_backend.controller;
 import com.eduhub.eduhub_backend.component.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,5 +24,33 @@ public class StudentController {
         studentList.add(new Student(3,"Ragu","L"));
         studentList.add(new Student(4,"Raj","R"));
         return new ResponseEntity<>(studentList,HttpStatus.OK);
+    }
+
+    @GetMapping("{id}/{firstName}/{lastName}")
+    public ResponseEntity<Student> studentPathVariable(
+            @PathVariable("id") int studentId,
+            @PathVariable("firstName") String firstName,
+            @PathVariable("lastName") String lastName){
+
+        Student student = new Student(studentId, firstName, lastName);
+
+        return ResponseEntity.ok(student);
+    }
+
+    @GetMapping("query")
+    public ResponseEntity<Student> studentRequestVariable(@RequestParam int studentId,
+                                                          @RequestParam String firstName,
+                                                          @RequestParam String lastName){
+        Student student = new Student(studentId, firstName, lastName);
+
+        return ResponseEntity.ok(student);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return ResponseEntity.ok(student);
     }
 }
